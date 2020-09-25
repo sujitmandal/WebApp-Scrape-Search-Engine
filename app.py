bed889f096f0222219d2369f5f5e052d52779fa1
sujit mandal <mandals974@gmail.com>
2020-09-25 17:40:20 +0530

    First commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ScrapeSearchEngine.ScrapeSearchEngine import Duckduckgo
 from ScrapeSearchEngine.ScrapeSearchEngine import Ecosia
 from ScrapeSearchEngine.ScrapeSearchEngine import Givewater
 from ScrapeSearchEngine.ScrapeSearchEngine import Bing
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, request, render_template
 
 #Github: https://github.com/sujitmandal
 #This programe is create by Sujit Mandal
@@ -14,14 +14,13 @@ LinkedIn : https://www.linkedin.com/in/sujit-mandal-91215013a/
 """
 
 userAgent = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36') #my user agent
-search = ('pyhon') #Enter Anything for Search
+#search = ('pyhon') #Enter Anything for Search
 
 app = Flask(__name__)
 
-'''
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-	return(render_template('home.html'))'''
+	return(render_template('home.html'))
 
 def dictionary(lists):
 	finalList = []
@@ -39,37 +38,42 @@ def dictionary(lists):
 	return(commonLinks)
 
 
-@app.route('/')
-def Links():
-	googleSearch = Google(search, userAgent)
-	duckduckgoSearch = Duckduckgo(search, userAgent)
-	givewaterSearch = Givewater(search, userAgent)
-	ecosiaSearch = Ecosia(search, userAgent)
-	bingSearch = Bing(search, userAgent)
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+	if request.method == 'POST':
+		search = request.form['txt_search']
 
-	googleSet = set(googleSearch)
-	duckduckgoSet = set(duckduckgoSearch)
-	givewaterSet = set(givewaterSearch)
-	ecosiaSet = set(ecosiaSearch)
-	bingSet = set(bingSearch)
+		googleSearch = Google(search, userAgent)
+		duckduckgoSearch = Duckduckgo(search, userAgent)
+		givewaterSearch = Givewater(search, userAgent)
+		ecosiaSearch = Ecosia(search, userAgent)
+		bingSearch = Bing(search, userAgent)
 
-	intersection1 = googleSet.intersection(givewaterSet)
-	intersection2 = intersection1.intersection(duckduckgoSet)
-	intersection3 = intersection2.intersection(ecosiaSet)
-	intersection4 = intersection3.intersection(bingSet)
+		googleSet = set(googleSearch)
+		duckduckgoSet = set(duckduckgoSearch)
+		givewaterSet = set(givewaterSearch)
+		ecosiaSet = set(ecosiaSearch)
+		bingSet = set(bingSearch)
 
-	intersectionList = list(intersection4)
+		intersection1 = googleSet.intersection(givewaterSet)
+		intersection2 = intersection1.intersection(duckduckgoSet)
+		intersection3 = intersection2.intersection(ecosiaSet)
+		intersection4 = intersection3.intersection(bingSet)
 
-	googoleLinks = dictionary(googleSearch)
-	duckduckgoLinks = dictionary(duckduckgoSearch)
-	givewaterLinks = dictionary(givewaterSearch)
-	ecosiaLinks = dictionary(ecosiaSearch)
-	bingLinks = dictionary(bingSearch)
+		intersectionList = list(intersection4)
 
-	commonLinks = dictionary(intersectionList)
+		googoleLinks = dictionary(googleSearch)
+		duckduckgoLinks = dictionary(duckduckgoSearch)
+		givewaterLinks = dictionary(givewaterSearch)
+		ecosiaLinks = dictionary(ecosiaSearch)
+		bingLinks = dictionary(bingSearch)
 
-	return(render_template('base.html', googoleLinks=googoleLinks, duckduckgoLinks=duckduckgoLinks, givewaterLinks=givewaterLinks, ecosiaLinks=ecosiaLinks,bingLinks=bingLinks, commonLinks=commonLinks))
+		commonLinks = dictionary(intersectionList)
 
+		return(render_template('result.html', googoleLinks=googoleLinks, duckduckgoLinks=duckduckgoLinks, givewaterLinks=givewaterLinks, ecosiaLinks=ecosiaLinks,bingLinks=bingLinks, commonLinks=commonLinks))
+	return(None)
 
 if __name__ == "__main__":
-	app.run(port=8000)
+	app.run(
+		port=8000
+		)
--- static/js/main.js
+++ static/js/main.js
@@ -1,54 +1,15 @@
-$(document).ready(function () {
-    // Init
-    $('.image-section').hide();
-    $('.loader').hide();
-    $('#predictions').hide();
-
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (f) {
-                $('#imagePreview').css('background-image', 'url(' + f.target.predictions + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
+	function openCity(evt, search_eng_name) {
+	  var i, tabcontent, tablinks;
+	  tabcontent = document.getElementsByClassName("tabcontent");
+	  for (i = 0; i < tabcontent.length; i++) {
+	    tabcontent[i].style.display = "none";
+	  }
+	  tablinks = document.getElementsByClassName("tablinks");
+	  for (i = 0; i < tablinks.length; i++) {
+	    tablinks[i].className = tablinks[i].className.replace(" active", "");
+	  }
+	  document.getElementById(search_eng_name).style.display = "block";
+	  evt.currentTarget.className += " active";
     }
-    $("#imageUpload").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-        $('#predictions').text('');
-        $('#predictions').hide();
-        readURL(this);
-    });
-
-    // Predict
-    $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
-
-        // Show loading animation
-        $(this).hide();
-        $('.loader').show();
-
-        // Make prediction by calling api /predict
-        $.ajax({
-            type: 'POST',
-            url: '/predict',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            async: true,
-            success: function (data) {
-                // Get and display the result
-                $('.loader').hide();
-                $('#predictions').fadeIn(600);
-                $('#predictions').text('predictions :  ' + data);
-                console.log('Success!');
-            },
-        });
-    });
-
-});
+    
+    
